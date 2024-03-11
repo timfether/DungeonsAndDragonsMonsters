c18e0a28b4774cd9dfbf41d504a8d5cdce152e21
Tim Fether <timfether@gmail.com>
2024-03-10 20:11:24 -0400

    Initial Commit

---
+++ DungeonsAndDragonsMonsters/DungeonsAndDragonsMonstersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonsAndDragonsMonstersApp.swift
+//  DungeonsAndDragonsMonsters
+//
+//  Created by Tim Fether on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonsAndDragonsMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

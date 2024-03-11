7a751570261deb3a3d029274406bb44738c47361
Tim Fether <timfether@gmail.com>
2024-03-11 09:26:10 -0400

    Add MonstersListView and view model as first view in app

---
+++ DungeonsAndDragonsMonsters/MonstersListView.swift
@@ -0,0 +1,20 @@
+//
+//  MonstersListView.swift
+//  DungeonsAndDragonsMonsters
+//
+//  Created by Tim Fether on 3/11/24.
+//
+
+import SwiftUI
+
+struct MonstersListView: View {
+    @State private var viewModel = ViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MonstersListView()
+}
---
+++ DungeonsAndDragonsMonsters/MonstersListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MonstersListViewModel.swift
+//  DungeonsAndDragonsMonsters
+//
+//  Created by Tim Fether on 3/11/24.
+//
+
+import Foundation
+
+extension MonstersListView {
+    @Observable class ViewModel {
+        
+    }
+}
